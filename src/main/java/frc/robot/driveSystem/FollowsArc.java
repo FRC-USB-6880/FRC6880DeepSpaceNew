974da8999d8e3fda9524cbf8ffe7bf862b6e0398
Janani Rajadurai <janani.rajadurai@gmail.com>
2019-02-08 16:07:24 -0500

    implementing FollowsArc

---
+++ src/main/java/frc/robot/driveSystem/FollowsArc.java
@@ -0,0 +1,12 @@
+package frc.robot.driveSystem;
+
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public interface FollowsArc {
+    public TalonSRX getLeft();
+    public TalonSRX getRight();
+    public double getDistance();
+    public Subsystem getRequiredSubsystem();
+}
