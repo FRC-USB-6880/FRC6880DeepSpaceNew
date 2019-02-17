f3b9e1b543f51a88d861f45e7f5be1420d22c3f0
Pranav Burugula <pranav.burugula@gmail.com>
2019-02-17 13:38:17 -0500

    Added Lift and implemented attachments in Robot class

---
+++ src/main/java/frc/robot/attachments/Lift.java
@@ -0,0 +1,23 @@
+package frc.robot.attachments;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import frc.robot.Robot;
+
+public class Lift {
+    private Robot robot;
+    private WPI_TalonSRX liftMotor;
+
+    public Lift(Robot robot){
+        this.robot = robot;
+        liftMotor = new WPI_TalonSRX(8);
+    }
+
+    public void move(double power){
+        liftMotor.set(power);
+    }
+
+    public int getEncoderCounts(){
+        return liftMotor.getSelectedSensorPosition();
+    }
+}
